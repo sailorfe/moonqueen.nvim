007731092d556200a889ba9e6d25987cc22a962f
spica <sailorfe@proton.me>
2025-01-25 06:21:17 -0500

    initial vim

--- shipwright_build.lua
+++
@@ -1,7 +0,0 @@
-local colorscheme = require("lua.lush_theme/moonqueen")
-local lushwright = require("shipwright.transform.lush")
-
-run(colorscheme,
-  lushwright.to_vimscript,
-  {append, {"set background=dark", "let g:colors_name='moonqueen'"}},
-  {overwrite, "colors/moonqueen.vim"})
